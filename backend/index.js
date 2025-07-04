7078e41e2ba0d38bffad4eadc6d43ccbbe88cc33
sam-use <wasifstark007@gmail.com>
2025-07-04 20:05:35 +0530

    Update CORS to allow deployed frontend on Render

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://instagram-clone-frontend-mamm.onrender.com"
+  ],
   credentials: true
 }));
 
